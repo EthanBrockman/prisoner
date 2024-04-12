b968fda95609c63903db488be78c996bb164058e
EthanBrockman <156472921+EthanBrockman@users.noreply.github.com>
2024-04-12 16:28:14 -0500

    Add files via upload

---
+++ Game_theory_GAME.py
@@ -0,0 +1,61 @@
+class Prisoner:
+	def __init__(self, amount_of_games):
+		self.amount_of_games = amount_of_games
+		
+	def game(self, P1_decision, P2_decision):
+		if P1_decision == 'B' and P2_decision == 'B':
+			print(f'They both betray, both get 2 years.\n')
+			Game_History_P1.append('B')
+			Game_History_P1.append('B')
+		elif P1_decision == 'B' and P2_decision == 'S':
+			print(f'Prisoner 1 is faces no charge, prisoner 2 gets 3 years\n')
+			Game_History_P1.append('B')
+			Game_History_P2.append('S')
+		elif P1_decision == 'S' and P2_decision == 'B':
+			print(f'Prisoner 2 is faces no charge, prisoner 1 gets 3 years\n')
+			Game_History_P1.append('S')
+			Game_History_P2.append('B')
+		elif P1_decision == 'S' and P2_decision == 'S':
+			print(f'They both stay silent, both get 1 year\n')
+			Game_History_P1.append('S')
+			Game_History_P2.append('S')
+		else:
+			print('there is an error')
+			
+	def play_many_games(self):
+		for i in range(self.amount_of_games):
+			self.game('B','B')
+			
+class Player:
+	def __init__(self):
+		pass
+	
+	
+	
+def always_betray(game_list):
+	pass
+def always_silent(game_list):
+	pass
+def tit_for_tat(game_list):
+	pass
+def beat_last(game_list):
+	pass
+def no_mercy(game_list):
+	pass
+def randy(game_list):
+	pass
+
+	
+	
+
+
+Game_History_P1  = []
+Game_History_P2 = []
+P1_sentence = 0
+P2_sentence = 0
+
+
+
+example_game = Prisoner(10)
+example_game.play_many_games()
+print(Game_History_P1)
---
+++ Game_theory_MAIN.py
@@ -0,0 +1,8 @@
+from Game_theory_GAME.py import *
+
+print(f'Game Theory is the study of strategy of rational agents and the strategies they use.\n')
+print('You are going to be playing a type of Game Theory game called Prisoners Dilemma.')
+print('In this game two crimnals are arrested, they can either stay silent and maintain')
+print('cooperation or betray the other with the promise of being let go if you do so.')
+print(f'\nScoring rules:  B = betray  S = Silent\n1.(BB) If both betray each prisoner serves 2 years\n2.(SS) If both stay silent they will both serve 1 year.\n3.(BS) If one betrays and one stays silent the player who betrayed will be let go and the player who stayed silent will serve 3 years')
+
