7d51c88cd691686dade78224d31b4c5ba9d45a87
EthanBrockman <156472921+EthanBrockman@users.noreply.github.com>
2024-04-15 19:07:16 -0500

    Update Game_theory_GAME.py

--- Game_theory_GAME.py
+++ Game_theory_GAME.py
@@ -1,5 +1,6 @@
 import random
 
+writing_file = open('game_data', 'w')
 Game_History_P1  = []
 Game_History_P2 = []
 P1_sentence = 0
@@ -11,46 +12,49 @@ class Prisoner:
 		
 	def game(self, P1_decision, P2_decision):
 		global Game_History_P1, Game_History_P2, P1_sentence, P2_sentence
-		# This function determines the outcome of the game and records the games history and players sentence
-		
 		print(P1_decision, P2_decision)
+		# This function determines the outcome of the game and records the games history and players sentence
 		if P1_decision == 'B' and P2_decision == 'B':
 			print(f'They both betray, both get 2 years.\n')
+			writing_file.write(f'They both betray, both get 2 years.\n')
 			Game_History_P1.append('B')
 			Game_History_P2.append('B')
 			P1_sentence += 2
 			P2_sentence += 2
 		elif P1_decision == 'B' and P2_decision == 'S':
 			print(f'Prisoner 1 faces no charge, prisoner 2 gets 3 years\n')
+			writing_file.write(f'Prisoner 1 faces no charge, prisoner 2 gets 3 years\n')
 			Game_History_P1.append('B')
 			Game_History_P2.append('S')
 			P2_sentence += 3
 		elif P1_decision == 'S' and P2_decision == 'B':
 			print(f'Prisoner 2 faces no charge, prisoner 1 gets 3 years\n')
+			writing_file.write(f'Prisoner 2 faces no charge, prisoner 1 gets 3 years\n')
 			Game_History_P1.append('S')
 			Game_History_P2.append('B')
 			P1_sentence += 3
 		elif P1_decision == 'S' and P2_decision == 'S':
 			print(f'They both stay silent, both get 1 year\n')
+			writing_file.write(f'They both stay silent, both get 1 year\n')
 			Game_History_P1.append('S')
 			Game_History_P2.append('S')
 			P1_sentence += 1
 			P2_sentence += 1
 		else:
 			print('there is an error')
-		
 	def play_many_games(self, P1, P2):
-		#performs mutiple games depending on the amount listed by the user
+		# performs mutiple games depending on the amount listed by the user
 		for i in range(self.amount_of_games):
 			self.game(P1.player_choice(), P2.player_choice())
+		print('View game_data.txt to see how many years you will serve and the game history')
 			
 class Player:
 	def __init__(self, choice, player_number):
-		self.decision_history = [] # creates an empty list to keep track of the player's decision in each round
 		self.choice = choice
 		self.player_number = player_number
 	
 	def player_choice(self):
+		# returns the choice of the player with the game history of the other player
 		global Game_History_P1, Game_History_P2
 		if self.player_number == 1:
 			if self.choice == 1:
@@ -119,4 +123,13 @@ def randy(game_list):
 	elif random_number >= 0.5:
 		return('S')
 
+def sentence1():
+	# returns player 1's sentence
+	global P1_sentence
+	return(P1_sentence)
 	
+def sentence2():
+	#returns player 2's sentence
+	global P2_sentence
+	return(P2_sentence)
+
